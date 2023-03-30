7da489cd4010e2a85a334309996c8b9e0841fa9b
kkaerrung <yujeong6231@naver.com>
2023-03-30 23:32:17 +0900

    네트워크

---
+++ 새 폴더/옹알이.js
@@ -0,0 +1,10 @@
+function solution(babbling) { 
+    var answer = 0; 
+    const a = ["aya", "ye", "woo", "ma"]; 
+
+    babbling.forEach(ba => { 
+        a.forEach(a => {
+            if(ba.includes(a)) 
+        })
+    })
+}
